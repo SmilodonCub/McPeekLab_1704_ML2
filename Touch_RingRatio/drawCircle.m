55c1d5d3674bd3e1480f17acc49e70b25b6dba98
SmilodonCub <smilodon138@gmail.com>
2021-04-14 16:38:24 -0400

    stimulus dim set in pix_per_dva + textfile init

--- Touch_RingRatio/drawCircle.m
+++ Touch_RingRatio/drawCircle.m
@@ -1,28 +1,50 @@
-function drawCircle(radius,LineWidth, centerSize, image_name, path_name)
-%x and y are the coordinates of the center of the circle
-%r is the radius of the circle
-%0.01 is the angle step, bigger values will draw the circle faster but
-%you might notice imperfections (not very smooth)
-x = 4;
-y = 4;
-largestC = 4;
-ang=0:0.01:2*pi; 
+function drawCircle(imsize_dva, LineWidth, centerSize, image_name, path_name, pix_per_deg)
+%draw a white circle on a black background
+%turn axes, label, etc off
+%save Position in pixels w/ correct size for stimulus
+
+%dscribe how to draw a circle
+x = 1;
+y = 1;
+unitSize = 1;
+ang=0:0.01:2*pi;
+radius = 1/2;
 xp=radius*cos(ang);
 yp=radius*sin(ang);
+%create an figure instance & plto the circle
 fig = figure();
 plot(x+xp,y+yp,'w','Linewidth', LineWidth);
-pad = 0.1;
-ylim( [ 0-pad, 2*largestC+pad ] );
-xlim( [ 0-pad, 2*largestC+pad ] );
+%make plot limits slightly larger so that circle does not get cropped
+pad = 0.01;
+ylim( [ 0-pad, 2*unitSize+pad ] );
+xlim( [ 0-pad, 2*unitSize+pad ] );
+%turn off tick labels + axes
 set(gca,'XTick',[]); 
 set(gca,'Yticklabel',[]); 
+%make axis square, so circle does not get distorted
 axis square;
+%make background as my coffee
 set(gcf,'color','k');
 set(gca,'Color','k', 'XColor', 'none', 'YColor', 'none' )
 hold on
+%add center fixation dot
 plot( x,y, 's', 'MarkerFaceColor', 'w', 'MarkerEdgeColor', 'w', 'MarkerSize', centerSize )
+%undo some Matlab fuckery:
 set(gcf, 'InvertHardcopy', 'off')
 image_path = strcat(path_name, image_name);
+%set(gca, 'units', 'normalized'); %Just making sure it's normalized
+%Tight = get(gca, 'TightInset');  %Gives you the bording spacing between plot box and any axis labels
+                                 %[Left Bottom Right Top] spacing
+%NewPos = [Tight(1) Tight(2) 1-Tight(1)-Tight(3) 1-Tight(2)-Tight(4)]; %New plot position [X Y W H]
+%set(gca, 'Position', NewPos);
+
+%here we set the axis size to render in the right number of pixels to
+%generate a stimulus the size we need for the touchscreen
+set(findall(fig,'Units','pixels'),'Units','normalized');
+fig.Units = 'pixels';
+figsize = imsize_dva * round( pix_per_deg );
+fig.Position = [0 0 figsize figsize];
+
 print(image_path, '-dtiff')
 end
 
--- Touch_RingRatio/drawManyCircles.m
+++ Touch_RingRatio/drawManyCircles.m
@@ -1,17 +1,17 @@
-function image_names = drawManyCircles( radii,LineWidth, centerSize, directory )
+function image_names = drawManyCircles( im_sizes, LineWidth, centerSize, directory, pix_per_deg )
 %DRAWMANYCIRCLES creates tiff images of ringtask stimuli
-%radii = vector of all radii need images for
+%im_sizes = vector of image sizes in degrees visual angle for a given task
 %directory = where to save the images
 
-image_names = cell( length( radii ),1 );
+image_names = cell( length( im_sizes ),1 );
 
-for radius = 1:length( radii ) 
+for radius = 1:length( im_sizes ) 
     %radii( radius )
-    image_name = strcat( 'circ_R', num2str( radii( radius ) ), ...
+    image_name = strcat( 'circ_R', num2str( im_sizes( radius ) ), ...
         '_LW', num2str( LineWidth ), 'cS', num2str( centerSize ) );
     %print( image_name )
     image_names{ radius,1 } = image_name;
-    drawCircle( radii( radius ),LineWidth, centerSize, image_name, directory );
+    drawCircle( im_sizes( radius ),LineWidth, centerSize, image_name, directory, pix_per_deg );
 end
 
 end
